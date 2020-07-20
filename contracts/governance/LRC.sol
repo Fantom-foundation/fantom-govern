0e0890894a83c38a42e6eac50f97aa77ed3293a1
Egor Lysenko <uprendis@gmail.com>
2020-07-20 10:47:53 +0300

    lrc: refactor constants and functions

--- contracts/governance/LRC.sol
+++ contracts/governance/LRC.sol
@@ -18,10 +18,9 @@ library LRC {
         VETO // "veto"
     }
 
-    uint256 constant OptionsNum = 5;
-    uint256 constant levelOfDesignation = OptionsNum - 1; //
-    uint256 constant maxScale = 5;
-    // mapping(uint256 => uint256) scales;
+    uint256 constant optionsNum = 5;
+    uint256 constant lowestVetoIdx = optionsNum - 1; // only last opinion is a veto
+    uint256 constant vetoExtraResistanceScale = 1;
 
     struct Opinion {
         bytes32 name;
@@ -32,7 +31,7 @@ library LRC {
         bytes32 name;
         uint256 arc;
         uint256 dw;
-        Opinion[OptionsNum] opinions;
+        Opinion[optionsNum] opinions;
         uint256 resistance;
         uint256 totalVotes;
         uint256 maxPossibleVotes;
@@ -43,59 +42,51 @@ library LRC {
         uint256 weight;
     }
 
-    // function addScale(uint256 scale, uint256 idx) public {
-    //    scales[idx] = scale;
-    // }
-
     function recalculate(LrcOption storage self) public {
         calculateARC(self);
         calculateDW(self);
     }
 
     function calculateARC(LrcOption storage self) public {
-        uint256 maxPossibleResistance = self.totalVotes * maxScale;
-        uint256 rebasedActualResistance = self.resistance * Decimal.unit();
-        self.arc = rebasedActualResistance / maxPossibleResistance;
+        uint256 maxPossibleResistance = self.totalVotes.mul(maxResistanceScale());
+        self.arc = self.resistance.mul(Decimal.unit()).div(maxPossibleResistance);
     }
 
     function calculateDW(LrcOption storage self) public {
-        uint256 totalDesignation;
-        for (uint256 i = levelOfDesignation; i < OptionsNum; i++) {
-            totalDesignation += self.opinions[i].totalVotes;
+        uint256 totalVeto;
+        for (uint256 i = lowestVetoIdx; i < optionsNum; i++) {
+            totalVeto = totalVeto.add(self.opinions[i].totalVotes);
         }
 
-        uint256 designationRebased = totalDesignation * Decimal.unit();
-        self.dw = designationRebased / self.totalVotes;
+        self.dw = totalVeto.mul(Decimal.unit()).div(self.totalVotes);
     }
 
-    function calculateRawCount(LrcOption storage self) public {
+    function getOpinionResistanceScale(uint256 opinionId) public pure returns(uint256) {
+        if (opinionId >= lowestVetoIdx) {
+            return opinionId + vetoExtraResistanceScale;
+        }
+        return opinionId;
+    }
 
+    function maxResistanceScale() public pure returns(uint256) {
+        return getOpinionResistanceScale(optionsNum - 1);
     }
 
     function addVote(LrcOption storage self, uint256 opinionId, uint256 weight) public {
-        require(opinionId < OptionsNum, "wrong opinion id");
+        require(opinionId < optionsNum, "wrong opinion id");
         self.opinions[opinionId].totalVotes += weight;
 
-        uint256 scale;
-        if (opinionId == OptionsNum - 1) {
-            scale = OptionsNum;
-        } else {
-            scale = opinionId;
-        }
+        uint256 scale = getOpinionResistanceScale(opinionId);
 
         self.totalVotes += weight;
         self.resistance += weight * scale;
     }
 
     function removeVote(LrcOption storage self, uint256 opinionId, uint256 weight) public {
-        require(opinionId < OptionsNum, "wrong opinion id");
+        require(opinionId < optionsNum, "wrong opinion id");
         self.opinions[opinionId].totalVotes -= weight;
 
-        uint256 scale;
-        if (opinionId == OptionsNum - 1) {
-            scale = OptionsNum;
-        }
-        scale = opinionId;
+        uint256 scale = getOpinionResistanceScale(opinionId);
 
         self.totalVotes -= weight;
         self.resistance -= weight * scale;
