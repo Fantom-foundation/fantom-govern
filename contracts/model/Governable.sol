03d7f7991d6420fffbaf279cda4b08a02b999ed1
Egor Lysenko <uprendis@gmail.com>
2020-09-23 19:13:17 +0700

    refactor Governable interface

--- contracts/model/Governable.sol
+++ contracts/model/Governable.sol
@@ -7,9 +7,9 @@ interface Governable {
     // Gets the total weight of voters
     function getTotalWeight() external view returns (uint256);
 
-    // Gets the voting weight of the specified address (own weight, received delegated weight)
-    function getWeight(address addr) external view returns (uint256, uint256);
+    // Gets the received delegated weight
+    function getReceivedWeight(address addr) external view returns (uint256);
 
     // Gets the voting weight which is delegated from the specified address to the specified address
-    function getDelegatedWeight(address from, address to) external view returns (uint256);
+    function getWeight(address from, address to) external view returns (uint256);
 }
