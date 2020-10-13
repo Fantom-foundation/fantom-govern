d3fe3bf9fa72d1cc0df573b861e8f0dec0aa0be4
Egor Lysenko <uprendis@gmail.com>
2020-10-13 21:11:58 +0700

    erase legacy tests

--- integration_tests/test.js
+++
@@ -1,6 +0,0 @@
-const DefaultTests = require('./default_tests/main');
-const Web3 = require('web3');
-const commander = require('commander');
-const program = new commander.Command();
-
-DefaultTests();
