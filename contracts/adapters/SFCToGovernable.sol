3e888ea349cb64c6bcc5ea0d626f058cba175a56
Ibrahim Arshad <38925737+ibrahim1023@users.noreply.github.com>
2022-08-11 13:50:21 +0400

    Update SFCToGovernable contract

--- contracts/adapters/SFCToGovernable.sol
+++ contracts/adapters/SFCToGovernable.sol
@@ -3,60 +3,47 @@ pragma solidity ^0.5.0;
 import "../model/Governable.sol";
 
 interface SFC {
-    function delegations(address _from, uint256 _toStakerID) external view returns (uint256 createdEpoch, uint256 createdTime,
-        uint256 deactivatedEpoch, uint256 deactivatedTime, uint256 amount, uint256 paidUntilEpoch, uint256 toStakerID);
+    function getStake(address _from, uint256 _toValidatorID) external view returns (uint256);
 
-    function stakers(uint256 _stakerID) external view returns (uint256 status, uint256 createdEpoch, uint256 createdTime,
-        uint256 deactivatedEpoch, uint256 deactivatedTime, uint256 stakeAmount, uint256 paidUntilEpoch, uint256 delegatedMe, address dagAddress, address sfcAddress);
+    function getValidator(uint256 _validatorID) external view returns (uint256 status, uint256 deactivatedTime, uint256 deactivatedEpoch,
+        uint256 receivedStake, uint256 createdEpoch, uint256 createdTime, address auth);
 
-    function getStakerID(address _addr) external view returns (uint256);
+    function getValidatorID(address _addr) external view returns (uint256);
 
-    function stakeTotalAmount() external view returns (uint256);
-
-    function delegationsTotalAmount() external view returns (uint256);
+    function totalActiveStake() external view returns (uint256);
 }
 
 contract SFCToGovernable is Governable {
-    SFC internal sfc = SFC(address(0xFC00FACE00000000000000000000000000000000));
+    SFC internal sfc = SFC(address(0xA87c1a650D8aCEfcf017b3Ef480ece942E1BF02b));
 
     // Gets the total weight of voters
     function getTotalWeight() external view returns (uint256) {
-        return sfc.stakeTotalAmount() + sfc.delegationsTotalAmount();
+        return sfc.totalActiveStake();
     }
 
     // Gets the received delegated weight
     function getReceivedWeight(address addr) external view returns (uint256) {
-        uint256 stakerID = sfc.getStakerID(addr);
-        if (stakerID == 0) {
+        uint256 validatorID = sfc.getValidatorID(addr);
+        if (validatorID == 0) {
             return 0;
         }
-        (uint256 status, , , uint256 deactivatedEpoch, , uint256 stakeAmount, , uint256 delegatedMe, , address sfcAddress) = sfc.stakers(stakerID);
-        if (deactivatedEpoch != 0 || status != 0 || sfcAddress != addr) {
+        (uint256 status, , , uint256 receivedStake, , ,) = sfc.getValidator(validatorID);
+        if (status != 0) {
             return 0;
         }
-        return delegatedMe + stakeAmount;
+        return receivedStake;
     }
 
     // Gets the voting weight which is delegated from the specified address to the specified address
     function getWeight(address from, address to) external view returns (uint256) {
-        uint256 toStakerID = sfc.getStakerID(to);
-        if (toStakerID == 0) {
+        uint256 toValidatorID = sfc.getValidatorID(to);
+        if (toValidatorID == 0) {
             return 0;
         }
-        (uint256 status, , , uint256 toDeactivatedEpoch, , uint256 toStakeAmount, , , , address sfcAddress) = sfc.stakers(toStakerID);
-        if (toDeactivatedEpoch != 0 || status != 0 || sfcAddress != to) {
+        (uint256 status, , , , , ,) = sfc.getValidator(toValidatorID);
+        if (status != 0) {
             return 0;
         }
-        if (from == to) {
-            // get staker weight
-            return toStakeAmount;
-        } else {
-            // get delegation weight
-            (, , uint256 deactivatedEpoch, , uint256 amount, ,) = sfc.delegations(from, toStakerID);
-            if (deactivatedEpoch != 0) {
-                return 0;
-            }
-            return amount;
-        }
+        return sfc.getStake(from, toValidatorID);
     }
 }
