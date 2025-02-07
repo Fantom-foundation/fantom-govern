1d705301e4576f8adfb20032da1a37fbdfb263b6
Petr Hanzl <petrhanzl33@gmail.com>
2025-02-07 13:26:22 +0100

    Use 3 bytes.

--- contracts/version/Version.sol
+++ contracts/version/Version.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.27;
 /// @notice The version info of the Governance contract
 contract Version {
     // @notice Returns the version.
-    function version() public pure returns (bytes4) {
-        return 0x00000002; // version 0.0.0.2
+    function version() public pure returns (bytes3) {
+        return 0x000002; // version 0.0.2
     }
 }
