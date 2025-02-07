763e9a24148265fc3d78622ece1de12fd16ac947
cabrador <84449820+cabrador@users.noreply.github.com>
2025-02-07 13:37:26 +0100

    Update version contract (#42)

--- contracts/version/Version.sol
+++ contracts/version/Version.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
-/// @dev The version info of this contract
+/// @notice The version info of the Governance contract
 contract Version {
-    // @dev Returns the version of this contract.
-    function version() public pure returns (bytes4) {
-        // version 00.0.2
-        return "0002";
+    // @notice Returns the version.
+    function version() public pure returns (bytes3) {
+        return 0x000002; // version 0.0.2
     }
 }
